03791a53f2618b1d847fb04b332062e5bf5f7749
235906 <235906@local>
2020-05-15 12:47:32 +0800

    fffuck

--- LSTM.py
+++ LSTM.py
@@ -9,10 +9,10 @@ else:
 class LSTMCell(nn.Module):
     def __init__(self, in_features, hide_features):
         super(LSTMCell, self).__init__()
-        self.Wi = nn.Linear(in_features=in_features, out_features=hide_features, bias=False)
-        self.Wf = nn.Linear(in_features=in_features, out_features=hide_features, bias=False)
-        self.Wo = nn.Linear(in_features=in_features, out_features=hide_features, bias=False)
-        self.Wc = nn.Linear(in_features=in_features, out_features=hide_features, bias=False)
+        self.Wi = nn.Linear(in_features=in_features, out_features=hide_features, bias=True)
+        self.Wf = nn.Linear(in_features=in_features, out_features=hide_features, bias=True)
+        self.Wo = nn.Linear(in_features=in_features, out_features=hide_features, bias=True)
+        self.Wc = nn.Linear(in_features=in_features, out_features=hide_features, bias=True)
 
         self.Ui = nn.Linear(in_features=hide_features, out_features=hide_features, bias=True)
         self.Uf = nn.Linear(in_features=hide_features, out_features=hide_features, bias=True)
@@ -34,8 +34,7 @@ class LSTMCell(nn.Module):
 class LSTMRnn(nn.Module):
     def __init__(self, in_features=1, hide_features=50, out_features=1):
         super(LSTMRnn, self).__init__()
-        self.h0 = torch.zeros(hide_features, dtype=torch.double).to(device)
-        self.c0 = torch.zeros(out_features, dtype=torch.double).to(device)
+        self.hide_features = hide_features
         self.lstm_cell1 = LSTMCell(in_features, hide_features)
         self.lstm_cell2 = LSTMCell(hide_features, hide_features)
         self.linear = nn.Linear(in_features=hide_features, out_features=out_features)
@@ -43,10 +42,10 @@ class LSTMRnn(nn.Module):
     def forward(self, seq, predict=0):
         seq = seq.chunk(seq.size(1), dim=1)
         out = []
-        ht_1 = self.h0
-        ct_1 = self.c0
-        ht_2 = self.h0
-        ct_2 = self.c0
+        ht_1 = torch.zeros(self.hide_features, dtype=torch.double).to(device)
+        ct_1 = torch.zeros(self.hide_features, dtype=torch.double).to(device)
+        ht_2 = torch.zeros(self.hide_features, dtype=torch.double).to(device)
+        ct_2 = torch.zeros(self.hide_features, dtype=torch.double).to(device)
         o = None
         for x in seq:
             ht_1, ct_1 = self.lstm_cell1(x, ht_1, ct_1)
@@ -54,7 +53,7 @@ class LSTMRnn(nn.Module):
             o = self.linear(ht_2)
             out += [o]
         for i in range(predict):
-            ht_1, ct_1 = self.lstm_cell1(x, ht_1, ct_1)
+            ht_1, ct_1 = self.lstm_cell1(o, ht_1, ct_1)
             ht_2, ct_2 = self.lstm_cell2(ht_1, ht_2, ct_2)
             o = self.linear(ht_2)
             out += [o]
